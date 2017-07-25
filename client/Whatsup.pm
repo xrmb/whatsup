6cea86881026b34ce27523e77ba56e8d52779d90
xrmb <github@xrmb.org>
2017-07-24 20:31:17 -0400

    add: local record method

--- client/Whatsup.pm
+++ client/Whatsup.pm
@@ -7,6 +7,7 @@ use Sys::Hostname;
 use HTTP::Tiny;
 use JSON::PP;
 use File::Temp;
+use DBI;
 
 
 
@@ -36,10 +37,52 @@ sub new
   $self->{app} = $args{app};
   $self->{auth} = $cfg->{auth} || warn('no auth');
   $self->{service} = $cfg->{service} || warn('no service');
+  $self->{db} = $cfg->{db};
   $self->{noqueue} = $args{noqueue} || 0;
 
   $inst = bless($self, $class);
   return $inst;
+}
+
+
+
+sub local_record
+{
+  my ($self, %args) = @_;
+  if(!ref($self)) { $self = __PACKAGE__->new(%args); }
+
+  my $host = $args{host} || $self->{host} || return (undef, __LINE__, 'need host');
+  my $app = $args{app} || return (undef, __LINE__, 'need app');
+  my $time = $args{time} || time();
+
+  my $dbh = DBI->connect('dbi:SQLite:dbname='.$self->{db}, '', '') || return (undef, __LINE__, 'connect error');
+
+  my $q = qq|INSERT INTO `records` (`host`, `app`, `time`, `received`, `key`, `value`) VALUES (?, ?, ?, ?, ?, ?)|;
+  my $sth = $dbh->prepare($q) || return (undef, __LINE__, 'prepare error');
+
+  my $any = 0;
+  foreach my $key (sort(keys(%args)))
+  {
+    next if($key =~ /^(app|host|time|auth)$/);
+    #warn join(', ', $host, $app, $time, time(), $key, int($args{$key}));
+    if(!$sth->execute($host, $app, $time, time(), $key, int($args{$key})))
+    {
+      warn($sth->errstr());
+    }
+
+    $any++;
+  }
+
+  if(!$any)
+  {
+    #warn join(', ', $host, $app, $time, time());
+    if(!$sth->execute($host, $app, $time, time(), undef, undef))
+    {
+      warn($sth->errstr());
+    }
+  }
+
+  return (undef, 0, 'ok');
 }
 
 
@@ -56,24 +99,37 @@ sub record
   }
   $args{time} ||= time();
 
-  my $json = encode_json(\%args);
 
-  my $uri = $self->{service}.'add/';
-
-  my $ua = new HTTP::Tiny();
-  my $res = $ua->post($uri, { headers => {'Content-Type' => 'application/json'}, content => $json});
-  #my $req = HTTP::Request->new('POST', $uri);
-  #$req->header('Content-Type' => 'application/json');
-  #$req->content($json);
-
-  #my $lwp = LWP::UserAgent->new();
-  #my $res = $lwp->request($req);
-
-  my $content = $res->{content};
-
-  if($res->{status} != 200)
+  my $err;
+  my $status;
+  my $content;
+  if($self->{db})
   {
-    warn(sprintf("service: %d %s %s\n", $res->{status}, $uri, $content));
+    (undef, $err, $status) = $self->local_record(%args);
+  }
+  else
+  {
+    my $json = encode_json(\%args);
+
+    my $uri = $self->{service}.'add/';
+
+    my $ua = new HTTP::Tiny();
+    my $res = $ua->post($uri, { headers => {'Content-Type' => 'application/json'}, content => $json});
+    #my $req = HTTP::Request->new('POST', $uri);
+    #$req->header('Content-Type' => 'application/json');
+    #$req->content($json);
+
+    #my $lwp = LWP::UserAgent->new();
+    #my $res = $lwp->request($req);
+
+    $content = $res->{content};
+    $status = $res->{status};
+    $err = $res->{status} != 200;
+  }
+
+  if($err)
+  {
+    warn(sprintf("service: %d %s\n", $status, $content));
     if(!$self->{noqueue})
     {
       delete($args{auth});
