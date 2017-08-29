765636bca2d28bbdb58858263452da36fceb10e1
xrmb <github@xrmb.org>
2017-08-29 01:23:30 +0000

    add: support for loldht and location name

--- client/dht22.pl
+++ client/dht22.pl
@@ -7,21 +7,54 @@ use Whatsup;
 
 use strict;
 
+my $test = grep(/test/, @ARGV);
 
 my $err = 0;
-my $pin = Whatsup->new->{'dht22_pin'};
+my $bin = Whatsup->new->{'dht22_bin'} || die 'need dht22_bin';
+my $pin = Whatsup->new->{'dht22_pin'} || die 'need dht22_pin';
+my $name = Whatsup->new->{'dht22_name'} || die 'need dht22_name';
 for(;;)
 {
-  my $dht22 = `/usr/local/bin/dht22 -p $pin 2>&1`;
-  if($dht22 =~ /Humidity: ([\d\.]+).*Temperature: ([\d\.]+)/s)
+  my $out;
+  if($bin =~ /lol/)
   {
-    #print("h: $1, t: $2\n");
-    Whatsup->record(app => 'dht22', temperature => $2*10, humidity => $1*10);
-    last;
+    $out = `$bin $pin 2>&1`;
+    if($out =~ /Humidity = (?<h>[\d\.]+).*Temperature = (?<t>[\d\.]+)/s)
+    {
+      if($test)
+      {
+        print("$name.humidity: $+{h}, $name.temperature: $+{t}\n");
+      }
+      else
+      {
+        Whatsup->record(app => 'dht22', "$name.temperature" => int($+{t}*10), "$name.humidity" => int($+{h}*10));
+      }
+      last;
+    }
+  }
+  elsif($bin =~ /dht22/)
+  {
+    $out = `$bin -p $pin 2>&1`;
+    if($out =~ /Humidity: (?<h>[\d\.]+).*Temperature: (?<t>[\d\.]+)/s)
+    {
+      if($test)
+      {
+        print("$name.humidity: $+{h}, $name.temperature: $+{t}\n");
+      }
+      else
+      {
+        Whatsup->record(app => 'dht22', "$name.temperature" => int($+{t}*10), "$name.humidity" => int($+{h}*10));
+      }
+      last;
+    }
+  }
+  else
+  {
+    die 'dht22_bin not supported';
   }
   sleep(1);
   $err++;
-  print(STDERR "error: $dht22\n");
+  print(STDERR "error: $out\n");
   last if($err > 5);
   next;
 }
