8af2bae5594391b420055ec6d504cdace887bebc
xrmb <github@xrmb.org>
2019-08-28 21:12:16 -0400

    change: support more than one sensor

--- client/dht22.pl
+++ client/dht22.pl
@@ -9,46 +9,72 @@ use strict;
 
 my $test = grep(/test/, @ARGV);
 
-my $err = 0;
 my $bin = Whatsup->new->{'dht22_bin'} || die 'need dht22_bin';
-my $pin = Whatsup->new->{'dht22_pin'} || die 'need dht22_pin';
-my $name = Whatsup->new->{'dht22_name'} || die 'need dht22_name';
-for(;;)
+my $pins = Whatsup->new->{'dht22_pin'} || die 'need dht22_pin';
+my $names = Whatsup->new->{'dht22_name'} || die 'need dht22_name';
+my $lock = Whatsup->new->{'dht22_lock'} == 0 ? 0 : 1;
+
+my @names = split(/,/, $names);
+my @pins = split(/,/, $pins);
+my %whatsup;
+SENSOR: for(;;)
 {
+  my $err = 0;
   my $out;
+  my $pin = shift(@pins);
+  my $name = shift(@names) || last;
 
-  if($bin =~ /lol/) { $out = `$bin $pin 2>&1`; }
-  elsif($bin =~ /dht22/) { $out = `$bin -p $pin 2>&1`; }
-  else { die 'dht22_bin not supported'; }
-
-  if($out =~ /Humidity = (?<h>[\d\.]+).*Temperature = (?<t>[\d\.]+)/s ||
-     $out =~ /Humidity: (?<h>[\d\.]+).*Temperature: (?<t>[\d\.]+)/s)
+  for(;;)
   {
-    my $t = $+{t}*1;
-    my $h = $+{h}*1;
-    if($h < 0 || $h > 100 || $t < -40 || $t > 80)
-    {
-      $out = sprintf("value out of range: $name.humidity: %.1f%%, $name.temperature: %.1fC\n", $t, $h);
+    if($bin =~ /lol/) 
+    { 
+      $out = `$bin $pin 5 $lock 2>&1`; 
     }
-    else
+  
+    elsif($bin =~ /dht22/) 
+    { 
+      $out = `$bin -p $pin 2>&1`; 
+    }
+
+    else 
+    { 
+      die 'dht22_bin not supported'; 
+    }
+
+    if($out =~ /Humidity = (?<h>[\d\.]+).*Temperature = (?<t>[\d\.]+)/s ||
+       $out =~ /Humidity: (?<h>[\d\.]+).*Temperature: (?<t>[\d\.]+)/s)
     {
-      if($test)
+      my $t = $+{t}*1;
+      my $h = $+{h}*1;
+      if($h < 0 || $h > 100 || $t < -40 || $t > 80)
       {
-        printf("$name.humidity: %.1f%%, $name.temperature: %.1fC\n", $h, $t);
+        $out = sprintf("value out of range: $name.humidity: %.1f%%, $name.temperature: %.1fC\n", $t, $h);
       }
       else
       {
-        Whatsup->record(app => 'dht22', "$name.temperature" => int($t*10), "$name.humidity" => int($h*10));
+        if($test)
+        {
+          printf("$name.humidity: %.1f%%, $name.temperature: %.1fC\n", $h, $t);
+        }
+        else
+        {
+          %whatsup = (%whatsup, app => 'dht22', "$name.temperature" => int($t*10), "$name.humidity" => int($h*10));
+        }
+        last;
       }
-      last;
     }
-  }
 
-  sleep(1);
-  $err++;
-  print(STDERR "error: $out\n");
-  last if($err > 5);
-  next;
+    sleep(1);
+    $err++;
+    print(STDERR "error: $out\n");
+    last if($err > 5);
+    next;
+  }
+}
+
+if(%whatsup)
+{
+  Whatsup->record(%whatsup);
 }
 
 exit 0;
