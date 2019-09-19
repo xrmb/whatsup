e800fc5a5703f9df4808fba7645f8991b51e9d87
xrmb <github@xrmb.org>
2019-09-19 03:28:07 +0100

    change: make it compile on linux

--- client/test.pl
+++ client/test.pl
@@ -1,8 +1,12 @@
-#!perl
+#!/usr/bin/env perl
+
+use Cwd;
+use lib (Cwd::abs_path(__FILE__.'/..'));
+
+use Whatsup;
 
 use strict;
 
-use Whatsup;
 
 my ($r, $c) = Whatsup->record(app => 'test', ping => 1);
 print("result:\t$r\n");
